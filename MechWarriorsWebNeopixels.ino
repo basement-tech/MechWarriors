51661a11b743e7cbc2328869acd6ec66473d8b1c
Daniel Zimmerman <djzman078@gmail.com>
2025-02-04 14:47:53 -0600

    added OTA updates - tested: works

--- MechWarriorsWebNeopixels.ino
+++ MechWarriorsWebNeopixels.ino
@@ -136,6 +136,12 @@
  *    Send the new firmware:
  *    Select the new firmware file you want to upload. 
  *    Initiate the update process through the web interface. 
+ *
+ *    Uploading Firmware OTA
+ *      Make sure your ESP is connected to the same network.
+ *      Open Arduino IDE → Tools → Port.
+ *      Select the ESP's network address (e.g., esp8266-XXXX.local).
+ *      Click Upload → The new firmware will be uploaded via OTA!
  * 
  *
  * (c) Daniel J. Zimmerman  Jan 2025
@@ -158,6 +164,7 @@
 
 #include <FS.h>        // File System for Web Server Files
 #include <LittleFS.h>  // This file system is used.
+#include <ArduinoOTA.h>  // Over-the-air updates
 
 #include "bt_eepromlib.h"
 #include "neo_data.h"  // for neopixels
@@ -456,6 +463,7 @@ protected:
 };
 
 
+
 // SETUP everything to make the webserver work.
 void setup(void) {
   delay(3000);  // wait for serial monitor to start completely.
@@ -574,6 +582,55 @@ void setup(void) {
     TRACE("Error connecting to %s\n", ssid);
 
 
+  /*
+  * OTA callbacks (from ArduinoOTA example)
+  */
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else {  // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+// end of OTA callbacks
+  ArduinoOTA.begin();
 
 
   // Ask for the current time using NTP request builtin into ESP firmware.
@@ -654,6 +711,7 @@ void setup(void) {
 // run the server...
 void loop(void) {
   server.handleClient();
+  ArduinoOTA.handle();
   neo_cycle_next();
 }  // loop()
 
